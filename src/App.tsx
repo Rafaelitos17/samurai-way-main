3b85a16282a3b9d620139635bf620fb970006781
Rafael <rafael.amoyan@mail.ru>
2022-04-27 12:21:29 +0300

    Change readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Header";
+import {Navbar} from "./Navbar";
+import {Profile} from "./Profile";
+
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <Profile/>
+        </div>
+    );
 }
 
 export default App;
